8ecf0cd0939d3a13422cdf5293a1ee10de3c363c
신재근 <jay@sinjaegeun-ui-MacBookPro.local>
2025-09-13 00:34:57 +0900

    Quartz sync: Sep 13, 2025, 12:34 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "기록 뭐 그런거..?",
+    pageTitle: "🙋🏻‍♂️ Jay's Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
