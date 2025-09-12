f65f9cd6aab81df81015218aef3dc8d57e3c54cd
신재근 <jay@sinjaegeun-ui-MacBookPro.local>
2025-09-12 16:47:45 +0900

    Quartz sync: Sep 12, 2025, 4:47 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,20 +6,20 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [
-  Component.Comments({
-    provider: 'giscus',
-    options: {
-      // data-repo
-      repo: 'shinjay7/my-blog',
-      // data-repo-id
-      repoId: 'R_kgDOPr0xUA',
-      // data-category
-      category: 'Announcements',
-      // data-category-id
-      categoryId: 'DIC_kwDOPr0xUM4CvWL8',
-    }
-  }),
-],,
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // data-repo
+        repo: 'shinjay7/my-blog',
+        // data-repo-id
+        repoId: 'R_kgDOPr0xUA',
+        // data-category
+        category: 'Announcements',
+        // data-category-id
+        categoryId: 'DIC_kwDOPr0xUM4CvWL8',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
